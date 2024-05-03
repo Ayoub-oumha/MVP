7feb08c31a08088476a8cbb10fb87bf9ad0e803c
ayoub-oumha <ayyouboumha@gmail.com>
2024-05-03 13:23:19 +0100

    commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let chrono   = document.getElementById("chrono") ; 
+let start    = document.querySelector(".btn1") ;
+let stopBtn  = document.querySelector(".btn2") ;
+let rest     = document.querySelector(".btn3") ; 
+let mlSecond = 1 ;
+let [h , m , s , ms]= [0, 0 , 0 , 0] ;
+let [hour , minuts , secons , mlSecons] = [0, 0 , 0 , 0]  ;
+
+
+let Interval = null ;
+
+start.addEventListener("click" , ()=>{
+    Interval = setInterval(mainFunction , 10) ;
+
+}) ;
+
+
+let mainFunction = function(){
+    ms = ms+1 ;
+    if(ms == 100) {
+        s = s+1
+        ms = 0 ;
+    } ;
+    if(s == 60){
+        m = m + 1 ;
+        s = 0 ;
+    } ;
+    if(m == 60){
+        h = h + 1 ;
+        m = 0 ;
+    } ;
+    hour = h < 10 ? "0" + h : h ;
+    minuts = m < 10 ? "0" + m : m ;
+    secons = s < 10 ? "0" + s : s ;
+    mlSecons = ms < 10 ? "0" + ms : ms ;
+    chrono.textContent = `${hour}:${minuts}:${secons}:${mlSecons}`
+}
+stopBtn.addEventListener("click", function(){
+    clearInterval(Interval)
+})
+rest.addEventListener("click" , ()=>{ 
+    clearInterval(Interval)
+    ms = 0 ;
+    s = 0 ;
+    m = 0 ;
+    h = 0
+    chrono.textContent = `00:00:00:00`
+
+} ) ;
