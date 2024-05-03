d01b3d949144a943e91a8b154159d1d63d1839b5
ayoub-oumha <ayyouboumha@gmail.com>
2024-05-03 14:53:04 +0100

    commit

--- script/Chrono script.js
+++ script/Chrono script.js
@@ -2,10 +2,15 @@ let chrono   = document.getElementById("chrono") ;
 let start    = document.querySelector(".btn1") ;
 let stopBtn  = document.querySelector(".btn2") ;
 let rest     = document.querySelector(".btn3") ; 
+let saveBtn = document.querySelector(".btn4") ;
+
+let divChronosave = document.querySelector(".chorono-save") ;
+console.log(divChronosave)
+
+
 let mlSecond = 1 ;
 let [h , m , s , ms]= [0, 0 , 0 , 0] ;
 let [hour , minuts , secons , mlSecons] = [0, 0 , 0 , 0]  ;
-
 
 let Interval = null ;
 
@@ -45,5 +50,15 @@ rest.addEventListener("click" , ()=>{
     m = 0 ;
     h = 0
     chrono.textContent = `00:00:00:00`
+    divChronosave.textContent = ""
 
 } ) ;
+
+saveBtn.addEventListener("click" ,()=>{
+    
+    let  para = document.createElement("p");
+    
+    let  node = document.createTextNode(chrono.textContent);
+    para.appendChild(node);
+    divChronosave.appendChild(para);
+} )
